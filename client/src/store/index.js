9ca985dd11974b18625941034042027f792e1241
NahuelNancucheo <nahuel.nancucheo@outlook.com>
2021-07-09 21:45:14 -0300

    redux devtools fixed

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,12 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from "../reducer/index";
-import thunk from 'react-thunk';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import thunk from 'redux-thunk';
 
 const store = createStore(
     rootReducer,
-    composeEnhancers(applyMiddleware(thunk))
+    composeWithDevTools(applyMiddleware(thunk))
 );
+
 
 export default store;
